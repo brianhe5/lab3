6c8d5c3ddb9faccbd3d0ce68bc0c3dd9c2c8ad8d
Brian He <brianhek123@gmail.com>
2024-02-29 18:33:30 -0800

    new v2222

--- hash-table-v2.c
+++ hash-table-v2.c
@@ -97,30 +97,31 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
 
 	//get notion of given that bucket, get buckets head (start of linked list)
 	//LOCK HERE B/C 2 THREADS OF SAME BUCKET, MAY POINT TO SAME ONE
+	struct list_head *list_head = &hash_table_entry->list_head;
+	//get list entry, prop of hash table: no 2 elements can have same key, to prevent 2 nodes having same key, call this
+	//hash table key and head, given bucket that we were indexed into, do linear scan in that buckets LL, and check that key trying to insert doesnt exist in LL
+	//LOCK HERE FOR CASE THAT 2 WITH SAME KEY WILL THINK KEY DOES NOT EXIST YET
+	struct list_entry *list_entry = get_list_entry(hash_table, key, list_head);
+
+	/* Update the value if it already exists */
+	// 2 of same key cant exist
+	//insetad of creating new node, we find node through get list entry, override value to value we are trying to insert
+	//exit out of function as soon as we do that
 	if (pthread_mutex_lock(&mutex2) != 0)
 	{
 		int err = errno;
 		perror("lock2");
 		exit(err);
 	}
-	struct list_head *list_head = &hash_table_entry->list_head;
-	//get list entry, prop of hash table: no 2 elements can have same key, to prevent 2 nodes having same key, call this
-	//hash table key and head, given bucket that we were indexed into, do linear scan in that buckets LL, and check that key trying to insert doesnt exist in LL
-	//LOCK HERE FOR CASE THAT 2 WITH SAME KEY WILL THINK KEY DOES NOT EXIST YET
-	struct list_entry *list_entry = get_list_entry(hash_table, key, list_head);
+	if (list_entry != NULL) {
+		list_entry->value = value;
+		return;
+	}
 	if(pthread_mutex_unlock(&mutex2) != 0)
 	{
 		int err = errno;
 		perror("unlock2");
 		exit(err);
-	}
-	/* Update the value if it already exists */
-	// 2 of same key cant exist
-	//insetad of creating new node, we find node through get list entry, override value to value we are trying to insert
-	//exit out of function as soon as we do that
-	if (list_entry != NULL) {
-		list_entry->value = value;
-		return;
 	}
 	//if list entry is null, creates a new node for new entry
 	//no same key, allocate memory for new ndode

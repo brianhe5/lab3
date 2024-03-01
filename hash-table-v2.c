6811e7d733e8b1d006e4fa04dfdb9e576540491b
Brian He <brianhek123@gmail.com>
2024-02-29 21:25:29 -0800

    calloc outside 1 mutex (mutex2)

--- hash-table-v2.c
+++ hash-table-v2.c
@@ -17,7 +17,7 @@ struct list_entry {
 
 SLIST_HEAD(list_head, list_entry);
 static pthread_mutex_t mutex2;
-static pthread_mutex_t mutex3;
+// static pthread_mutex_t mutex3;
 
 struct hash_table_entry {
 	struct list_head list_head;
@@ -41,12 +41,12 @@ struct hash_table_v2 *hash_table_v2_create()
 		perror("init2");
 		exit(err);
 	}
-	if (pthread_mutex_init(&mutex3, NULL) != 0)
-	{
-		int err = errno;
-		perror("init3");
-		exit(err);
-	}
+	// if (pthread_mutex_init(&mutex3, NULL) != 0)
+	// {
+	// 	int err = errno;
+	// 	perror("init3");
+	// 	exit(err);
+	// }
 	return hash_table;
 }
 
@@ -74,7 +74,7 @@ static struct list_entry *get_list_entry(struct hash_table_v2 *hash_table,
 	}
 	return NULL;
 }
-
+//CANT CHANGE
 bool hash_table_v2_contains(struct hash_table_v2 *hash_table,
                             const char *key)
 {
@@ -90,12 +90,12 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
 {
 	//3 param: hash table, key-value pair
 	//pass in hash table as well as key: given table, take in key, pass through hash func, get index into hash table corresponding to key
-	//DONT LOCK HERE B/C JUST GETTING INDEX OF HASH TABLE ITSELF
-
+	//DONT LOCK HERE B/C DOESNT BLOCK JUST 1 BUCKET
+	//INSTEAD BLOCKS ALL THREADS FROM TRYIGNT O ACCESS ANY BUCKET
 	struct hash_table_entry *hash_table_entry = get_hash_table_entry(hash_table, key);
 
 	//get notion of given that bucket, get buckets head (start of linked list)
-	//LOCK HERE B/C 2 THREADS OF SAME BUCKET, MAY POINT TO SAME ONE
+	//DONT BLOCK HERE B/C OTHER THREADS MAY LOOK FOR HEAD OF ANOTHER BUCKET
 	struct list_head *list_head = &hash_table_entry->list_head;
 
 	//get list entry, prop of hash table: no 2 elements can have same key, to prevent 2 nodes having same key, call this
@@ -107,6 +107,9 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
 	// 2 of same key cant exist
 	//insetad of creating new node, we find node through get list entry, override value to value we are trying to insert
 	//exit out of function as soon as we do that
+
+	list_entry = calloc(1, sizeof(struct list_entry));
+
 	if (pthread_mutex_lock(&mutex2) != 0)
 	{
 		int err = errno;
@@ -117,37 +120,36 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
 		list_entry->value = value;
 		return;
 	}
+
+	//if list entry is null, creates a new node for new entry
+	//no same key, allocate memory for new ndode
+	//assign key and value, insert into LL now
+
+	// if (pthread_mutex_lock(&mutex3) != 0)
+	// {
+	// 	int err = errno;
+	// 	perror("lock3");
+	// 	exit(err);
+	// }
+	list_entry->key = key;
+	list_entry->value = value;
+	//LOCK HERE FOR CASE OF POSSIBLE INSERTION TO SAME HEAD
+
+	SLIST_INSERT_HEAD(list_head, list_entry, pointers);
 	if(pthread_mutex_unlock(&mutex2) != 0)
 	{
 		int err = errno;
 		perror("unlock2");
 		exit(err);
 	}
-
-	//if list entry is null, creates a new node for new entry
-	//no same key, allocate memory for new ndode
-	//assign key and value, insert into LL now
-
-	if (pthread_mutex_lock(&mutex3) != 0)
-	{
-		int err = errno;
-		perror("lock3");
-		exit(err);
-	}
-	list_entry = calloc(1, sizeof(struct list_entry));
-	list_entry->key = key;
-	list_entry->value = value;
-	//LOCK HERE FOR CASE OF POSSIBLE INSERTION TO SAME HEAD
-
-	SLIST_INSERT_HEAD(list_head, list_entry, pointers);
-	if(pthread_mutex_unlock(&mutex3) != 0)
-	{
-		int err = errno;
-		perror("unlock3");
-		exit(err);
-	}
+	// if(pthread_mutex_unlock(&mutex3) != 0)
+	// {
+	// 	int err = errno;
+	// 	perror("unlock3");
+	// 	exit(err);
+	// }
 }
-
+//CANT CHAGE
 uint32_t hash_table_v2_get_value(struct hash_table_v2 *hash_table,
                                  const char *key)
 {
@@ -166,12 +168,12 @@ void hash_table_v2_destroy(struct hash_table_v2 *hash_table)
 		perror("destroy2");
 		exit(err);
 	}
-	if (pthread_mutex_destroy(&mutex3) != 0)
-	{
-		int err = errno;
-		perror("destroy3");
-		exit(err);
-	}
+	// if (pthread_mutex_destroy(&mutex3) != 0)
+	// {
+	// 	int err = errno;
+	// 	perror("destroy3");
+	// 	exit(err);
+	// }
 	for (size_t i = 0; i < HASH_TABLE_CAPACITY; ++i) {
 		struct hash_table_entry *entry = &hash_table->entries[i];
 		struct list_head *list_head = &entry->list_head;

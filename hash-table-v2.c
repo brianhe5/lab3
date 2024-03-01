75c701898326e0d033736143401e9e50df1cd031
Brian He <brianhek123@gmail.com>
2024-02-29 18:55:19 -0800

    prayge

--- hash-table-v2.c
+++ hash-table-v2.c
@@ -98,12 +98,7 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
 	//get notion of given that bucket, get buckets head (start of linked list)
 	//LOCK HERE B/C 2 THREADS OF SAME BUCKET, MAY POINT TO SAME ONE
 	struct list_head *list_head = &hash_table_entry->list_head;
-	if (pthread_mutex_lock(&mutex2) != 0)
-	{
-		int err = errno;
-		perror("lock2");
-		exit(err);
-	}
+
 	//get list entry, prop of hash table: no 2 elements can have same key, to prevent 2 nodes having same key, call this
 	//hash table key and head, given bucket that we were indexed into, do linear scan in that buckets LL, and check that key trying to insert doesnt exist in LL
 	//LOCK HERE FOR CASE THAT 2 WITH SAME KEY WILL THINK KEY DOES NOT EXIST YET
@@ -113,6 +108,12 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
 	// 2 of same key cant exist
 	//insetad of creating new node, we find node through get list entry, override value to value we are trying to insert
 	//exit out of function as soon as we do that
+	if (pthread_mutex_lock(&mutex2) != 0)
+	{
+		int err = errno;
+		perror("lock2");
+		exit(err);
+	}
 	if (list_entry != NULL) {
 		list_entry->value = value;
 		return;
@@ -126,16 +127,17 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
 	//if list entry is null, creates a new node for new entry
 	//no same key, allocate memory for new ndode
 	//assign key and value, insert into LL now
-	list_entry = calloc(1, sizeof(struct list_entry));
-	list_entry->key = key;
-	list_entry->value = value;
-	//LOCK HERE FOR CASE OF POSSIBLE INSERTION TO SAME HEAD
 	if (pthread_mutex_lock(&mutex3) != 0)
 	{
 		int err = errno;
 		perror("lock3");
 		exit(err);
 	}
+	list_entry = calloc(1, sizeof(struct list_entry));
+	list_entry->key = key;
+	list_entry->value = value;
+	//LOCK HERE FOR CASE OF POSSIBLE INSERTION TO SAME HEAD
+
 	SLIST_INSERT_HEAD(list_head, list_entry, pointers);
 	if(pthread_mutex_unlock(&mutex3) != 0)
 	{
